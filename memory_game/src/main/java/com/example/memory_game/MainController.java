20a9c91d85e4fc893a69bc05226bc544d295b3b0
CasianLW <ciorbcasian@gmail.com>
2023-05-10 20:13:13 +0200

    Controller du front, affichage de la fenetre d'accueil

---
+++ memory_game/src/main/java/com/example/memory_game/MainController.java
@@ -0,0 +1,51 @@
+package com.example.memory_game;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class MainController {
+
+    @FXML
+    private ComboBox<String> themeComboBox;
+    @FXML
+    private ComboBox<String> difficultyComboBox;
+    @FXML
+    private TextField player1NameTextField;
+    @FXML
+    private TextField player2NameTextField;
+    @FXML
+    private Button startGameButton;
+
+    @FXML
+    public void initialize() {
+        // Ajoutez les thèmes et les difficultés à leurs ComboBox respectives
+        themeComboBox.getItems().addAll("StarWars", "Theme 2", "Theme 3");
+        difficultyComboBox.getItems().addAll("Easy", "Medium", "Hard");
+
+        // Désactivez le bouton "Start Game" si les noms des joueurs sont vides
+        startGameButton.disableProperty().bind(
+                player1NameTextField.textProperty().isEmpty().or(
+                        player2NameTextField.textProperty().isEmpty())
+        );
+    }
+
+    @FXML
+    public void onStartGameButtonClicked() {
+        // Récupérez les informations sélectionnées par l'utilisateur
+        String theme = themeComboBox.getSelectionModel().getSelectedItem();
+        String difficulty = difficultyComboBox.getSelectionModel().getSelectedItem();
+        String player1Name = player1NameTextField.getText();
+        String player2Name = player2NameTextField.getText();
+
+        // Initialisez la partie en fonction des informations récupérées
+        // (vous devrez adapter les paramètres en fonction de votre implémentation de la classe Game)
+        Game game = new Game(theme, difficulty, player1Name, player2Name);
+
+        // Lancez le jeu et fermez la fenêtre principale
+        game.start();
+
+        Stage stage = (Stage) startGameButton.getScene().getWindow();
+        stage.close();
+    }
+}
