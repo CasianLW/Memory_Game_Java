1d4fba3676598b606843396c11d8e57baf747f75
CasianLW <ciorbcasian@gmail.com>
2023-05-10 17:29:00 +0200

    player stats class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,30 @@
 public class Player {
+
+    private String name;
+    private int points;
+
+    public Player(String name) {
+        this.name = name;
+        this.points = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public void addPoint() {
+        this.points++;
+    }
 }
